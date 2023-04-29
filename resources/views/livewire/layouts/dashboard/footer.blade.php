d1d8a5049e64eb8708c79af60ac70afa1a555f98
mgalal <mgalal8866@gmail.com>
2023-04-29 23:14:17 +0200

    modified:   resources/views/livewire/layouts/dashboard/footer.blade.php

--- resources/views/livewire/layouts/dashboard/footer.blade.php
+++ resources/views/livewire/layouts/dashboard/footer.blade.php
@@ -1,6 +1,6 @@
 <div>
    <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2021<a class="ms-25" href="" target="_blank">MG</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span>
+    <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2023<a class="ms-25" href="" target="_blank">MG</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span>
         {{-- <span class="float-md-end d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span> --}}
     </p>
 </footer>
