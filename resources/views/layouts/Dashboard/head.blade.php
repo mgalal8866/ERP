e8913d075f556f6c6b2a2bead32e2a87c7f6c8b7
mgalal <mgalal8866@gmail.com>
2023-04-29 23:22:31 +0200

    modified:   resources/views/layouts/Dashboard/head.blade.php

--- resources/views/layouts/Dashboard/head.blade.php
+++ resources/views/layouts/Dashboard/head.blade.php
@@ -7,11 +7,12 @@
     {{-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet"> --}}
     <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/editors/quill/quill.snow.css')}}>
     <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/extensions/sweetalert2.min.css')}}>
+    <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/forms/select/select2.min.css')}}>
+    <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/extensions/toastr.min.css')}}>
 
     @if (LaravelLocalization::getCurrentLocaleDirection() == 'rtl' )
     <!-- BEGIN: Vendor CSS-->
         <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/vendors-rtl.min.css')}}>
-        <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/forms/select/select2.min.css')}}>
         <!-- END: Vendor CSS-->
 
 
@@ -23,7 +24,6 @@
         <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css-rtl/themes/dark-layout.css')}}>
         <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css-rtl/themes/bordered-layout.css')}}>
         <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css-rtl/themes/semi-dark-layout.css')}}>
-        <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/extensions/toastr.min.css')}}>
         <!-- BEGIN: Page CSS-->
         <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css-rtl/core/menu/menu-types/vertical-menu.css')}}>
         <!-- END: Page CSS-->
@@ -33,9 +33,9 @@
         <link rel="stylesheet" type="text/css" href={{URL::asset('assets/css/style-rtl.css')}}>
         <!-- END: Custom CSS-->
 
-    @else
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/vendors-rtl.min.css')}}>
+        @else
+        <!-- BEGIN: Vendor CSS-->
+        <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/vendors-rtl.min.css')}}>
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
@@ -46,7 +46,7 @@
     <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css/themes/dark-layout.css')}}>
     <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css/themes/bordered-layout.css')}}>
     <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css/themes/semi-dark-layout.css')}}>
-    <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/vendors/css/extensions/toastr.min.css')}}>
+
 
     <!-- BEGIN: Page CSS-->
     <link rel="stylesheet" type="text/css" href={{URL::asset('app-assets/css/core/menu/menu-types/vertical-menu.css')}}>
