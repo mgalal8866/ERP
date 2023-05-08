4af40e243a5d33ac232e4b42957faf6bdb79fd69
mgalal8866 <mgalal8866@gmail.com>
2023-05-08 19:01:52 +0300

    modified:   resources/views/livewire/pos/pos.blade.php

--- resources/views/livewire/pos/pos.blade.php
+++ resources/views/livewire/pos/pos.blade.php
@@ -71,10 +71,8 @@
                 <div  class="col-12 col-md-6 ">
 
                     <x-label  for="name" :value="__('tran.customer')"  tooltip="اختار العميل" />
-                    {{-- <x-select2  for="name" :value="__('tran.customer')"  tooltip="اختار العميل" /> --}}
                     <div wire:ignore class=" d-flex " style="flex-warp:nwrap !important">
-                        <i  data-feather="user"></i>
-
+                        <span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>
                         <select class="select2 form-select" id="select2" >
                             <option value="">Walk-In Customer</option>
                             @foreach($customers as $customer)
